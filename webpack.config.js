f1868637851609e72b5d33673f69318393832f7e
Nipun Paradkar <nipun@kiprosh.com>
2021-02-06 23:56:11 +0530

    Add webpack for ES6 compilation

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  mode: "production",
+  output: {
+    libraryTarget: "commonjs2"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react"
+            ]
+          }
+        }
+      }
+    ]
+  }
+};
